eae61d4b8d58e499557778254bee48a0045b49cd
sadcitizen <hi@sadcitizen.me>
2018-05-12 22:04:01 +0500

    color rules

---
+++ rules/color.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'color-hex-case': 'lower', // specify lowercase for hex colors
+    'color-hex-length': 'long', // specify long notation for hex colors
+    'color-named': 'never', //  disallow named colors
+    'color-no-invalid-hex': true // disallow invalid hex colors
+};
