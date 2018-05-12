48deb3eb56044999592225e38ccc4f0547f17a34
sadcitizen <hi@sadcitizen.me>
2018-05-12 22:01:29 +0500

    value rules

---
+++ rules/value.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'value-keyword-case': 'lower', // specify lowercase for keywords values
+    'value-list-comma-newline-after': 'always-multi-line', // require a newline or disallow whitespace after the commas of value lists
+    'value-list-comma-space-after': 'always-single-line', // require a single space after the commas in single-line value lists
+    'value-list-comma-space-before': 'never', // disallow whitespace before the commas of value lists
+    'value-list-max-empty-lines': 0, // limit the number of adjacent empty lines within value lists
+    'value-no-vendor-prefix': true // disallow vendor prefixes for values
+};
