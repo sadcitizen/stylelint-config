df48fdbda0294bb71a780d17ae4b97e1b549031a
sadcitizen <hi@sadcitizen.me>
2018-05-13 00:16:39 +0500

    block rules

---
+++ rules/block.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'block-closing-brace-empty-line-before': 'never', // disallow an empty line before the closing brace of blocks
+    'block-closing-brace-newline-after': 'always', // require a newline after the closing brace of blocks
+    'block-closing-brace-newline-before': 'always-multi-line', // require a newline before the closing brace of blocks
+    'block-closing-brace-space-before': 'always-single-line', // require a single space before the closing brace of blocks
+    'block-closing-brace-space-after': 'always-single-line', // require a single space after the closing brace of blocks
+    'block-no-empty': true, // disallow empty blocks
+    'block-opening-brace-newline-after': 'always', // require a newline after the opening brace of blocks
+    'block-opening-brace-newline-before': 'always-single-line', // require a newline before the opening brace of blocks
+    'block-opening-brace-space-before': 'always', // require a single space before the opening brace of blocks
+    'block-opening-brace-space-after': 'always-single-line' // require a single space after the opening brace of blocks
+};
