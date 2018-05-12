801828ee32330a114356b16e0e22a22c81be9609
sadcitizen <hi@sadcitizen.me>
2018-05-12 22:03:04 +0500

    unit rules

---
+++ rules/unit.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'length-zero-no-unit': true, // disallow units for zero lengths
+    'unit-case': 'lower', // specify lowercase or uppercase for units
+    'unit-no-unknown': true, // disallow unknown units
+    'unit-whitelist': [ // specify a whitelist of allowed units
+        'em',
+        'rem',
+        'px'
+    ]
+};
