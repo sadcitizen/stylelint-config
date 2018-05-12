10c4f9a46be1e69413c2f4248c1e1352edbab846
sadcitizen <hi@sadcitizen.me>
2018-05-12 22:04:52 +0500

    number rules

---
+++ rules/number.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'number-leading-zero': 'always', // require a leading zero for fractional numbers less than 1
+    'number-max-precision': 4, // limit the number of decimal places allowed in numbers
+    'number-no-trailing-zeros': true // disallow trailing zeros in numbers
+};
