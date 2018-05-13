b1c1c01be333716761499587dc4d81f7bb4386bc
sadcitizen <hi@sadcitizen.me>
2018-05-13 18:28:13 +0500

    function rules

---
+++ rules/function.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'function-calc-no-unspaced-operator': true, // disallow an unspaced operator within calc functions
+    'function-comma-newline-after': 'never-multi-line', // disallow whitespace before the commas in multi-line functions
+    'function-comma-space-after': 'always-single-line', // require a single space after the commas of functions
+    'function-comma-space-before': 'never', // disallow whitespace before the commas of functions
+    'function-linear-gradient-no-nonstandard-direction': true, // disallow direction values in linear-gradient() calls that are not valid according to the standard syntax
+    'function-max-empty-lines': 0, // limit the number of adjacent empty lines within functions
+    'function-name-case': 'lower', // specify lowercase for function names
+    'function-parentheses-newline-inside': 'always-multi-line', // require newline inside the parentheses of multi-line functions
+    'function-parentheses-space-inside': 'never-single-line', // disallow whitespace inside the parentheses of single-line functions
+    'function-url-quotes': 'always', // require quotes for urls
+    'function-whitespace-after': 'always' // require whitespace after functions
+};
