9c4f6f5b3f030e75784bf0a0d05ab219007059f0
sadcitizen <hi@sadcitizen.me>
2018-05-13 19:22:00 +0500

    media rules

---
+++ rules/media.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'media-feature-colon-space-after': 'always', // require a single space after the colon in media features
+    'media-feature-colon-space-before': 'never', // disallow whitespace before the colon in media features
+    'media-feature-name-case': 'lower', // specify lowercase or uppercase for media feature names
+    'media-feature-name-no-unknown': true, // disallow unknown media feature names
+    'media-feature-name-no-vendor-prefix': true, // disallow vendor prefixes for media feature names
+    'media-feature-parentheses-space-inside': 'never', // disallow whitespace on the inside of the parentheses within media features
+    'media-feature-range-operator-space-after': 'always', // require a single space after the range operator in media features
+    'media-feature-range-operator-space-before': 'always', // require a single space before the range operator in media features
+    'media-query-list-comma-newline-after': 'always-multi-line', // require a newline after the commas in multi-line media query lists
+    'media-query-list-comma-newline-before': 'never-multi-line', // disallow whitespace before the commas in multi-line media query lists
+    'media-query-list-comma-space-after': 'always-single-line', // require a single space after the commas in single-line media query lists
+    'media-query-list-comma-space-before': 'never' // disallow whitespace before the commas of media query lists
+};
