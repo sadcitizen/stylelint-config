93af084b690a85bcbc417b8b918dcf92518dce90
sadcitizen <hi@sadcitizen.me>
2018-05-13 18:00:55 +0500

    font rules

---
+++ rules/font.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'font-family-name-quotes': 'always-where-recommended', // specify quotation marks should be used around font family names
+    'font-family-no-duplicate-names': true, // disallow duplicate font family names
+    'font-family-no-missing-generic-family-keyword': true, // disallow missing generic families in lists of font family names
+    'font-weight-notation': 'numeric' // require numeric font-weight values
+};
